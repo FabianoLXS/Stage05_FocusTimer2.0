c4ec4d018fa4f59e0f78e6d5382f2caf5a8c1560
Fabiano Luiz Santos <47800473+FabianoLXS@users.noreply.github.com>
2023-05-12 15:40:42 -0300

    commit

--- js/index.js
+++ js/index.js
@@ -10,29 +10,84 @@ const buttonSoundRain = document.querySelector('.rainButton')
 const buttonSoundCoffeeShop = document.querySelector('.coffeeShopButton')
 const buttonSoundFirePlace = document.querySelector('.firePlaceButton')
 
-//Button variables
-
-// buttonPlay.addEventListener("click", function () {
-//   // buttonPlay.classList.add("pressed")
-//   buttonPlay.classList.add("pressed")
-//   console.log(buttonPlay)
-// })
-
-
-//function Pressed Button Sound
-
-
-buttonSoundForest.addEventListener("click", function() {
-  pressButtonForest()
-  buttonSoundForest.classList.add("soundOn")
-
-})
-
-//Audio
+//Forest Audio
 const forestSound = new Audio(
   "./sound/Floresta.wav"
-)
+  )
+  
+  function forestSoundOn() {
+    forestSound.play()
+  }
 
-function pressButtonForest() {
-  forestSound.play()
+function forestSoundOff() {
+  forestSound.pause()
 }
+  buttonSoundForest.addEventListener("click", function() {
+    if (buttonSoundForest.classList.contains("soundOn")) {
+      forestSoundOff()
+      buttonSoundForest.classList.remove("soundOn")
+    } else {
+      forestSoundOn()
+      buttonSoundForest.classList.add("soundOn")
+    }
+  })
+
+  //Rain Audio
+  const rainSound = new Audio("./sound/Chuva.wav")
+
+  function rainSoundOn() {
+    rainSound.play()
+  }
+
+  function rainSoundOff() {
+    rainSound.pause()
+  }
+  buttonSoundRain.addEventListener("click", function () {
+    if (buttonSoundRain.classList.contains("soundOn")) {
+      rainSoundOff()
+      buttonSoundRain.classList.remove("soundOn")
+    } else {
+      rainSoundOn()
+      buttonSoundRain.classList.add("soundOn")
+    }
+  })
+
+  //CoffeeShop Audio
+  const coffeeShopSound = new Audio("./sound/Cafeteria.wav")
+
+  function coffeeShopSoundOn() {
+    coffeeShopSound.play()
+  }
+
+  function coffeeShopSoundOff() {
+    coffeeShopSound.pause()
+  }
+  buttonSoundCoffeeShop.addEventListener("click", function () {
+    if (buttonSoundCoffeeShop.classList.contains("soundOn")) {
+      coffeeShopSoundOff()
+      buttonSoundCoffeeShop.classList.remove("soundOn")
+    } else {
+      coffeeShopSoundOn()
+      buttonSoundCoffeeShop.classList.add("soundOn")
+    }
+  })
+
+  //FiePlace Audio
+  const firePlaceSound = new Audio("./sound/Lareira.wav")
+
+  function firePlaceSoundOn() {
+    firePlaceSound.play()
+  }
+
+  function firePlaceSoundOff() {
+    firePlaceSound.pause()
+  }
+  buttonSoundFirePlace.addEventListener("click", function () {
+    if (buttonSoundFirePlace.classList.contains("soundOn")) {
+      firePlaceSoundOff()
+      buttonSoundFirePlace.classList.remove("soundOn")
+    } else {
+      firePlaceSoundOn()
+      buttonSoundFirePlace.classList.add("soundOn")
+    }
+  })
