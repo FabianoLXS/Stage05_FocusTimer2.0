276ce83798959e13da8fea8d3496e7187d0aa16f
Fabiano Luiz Santos <47800473+FabianoLXS@users.noreply.github.com>
2023-05-12 16:30:28 -0300

    commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import elements from "./elements.js"
+import Sounds from "./sound.js"
 
-const Elements = elements({
+const elements = elements({
   buttonPlay,
   buttonStop,
   buttonPlus,
@@ -11,84 +12,11 @@ const Elements = elements({
   buttonSoundFirePlace,
 })
 
-//Forest Audio
-const forestSound = new Audio(
-  "./sound/Floresta.wav"
-  )
-  
-  function forestSoundOn() {
-    forestSound.play()
-  }
+const Sounds = Sounds({
+  Forest,
+  Rain,
+  CoffeeShop,
+  FirePlace,
+})
 
-function forestSoundOff() {
-  forestSound.pause()
-}
-  buttonSoundForest.addEventListener("click", function() {
-    if (buttonSoundForest.classList.contains("soundOn")) {
-      forestSoundOff()
-      buttonSoundForest.classList.remove("soundOn")
-    } else {
-      forestSoundOn()
-      buttonSoundForest.classList.add("soundOn")
-    }
-  })
-
-  //Rain Audio
-  const rainSound = new Audio("./sound/Chuva.wav")
-
-  function rainSoundOn() {
-    rainSound.play()
-  }
-
-  function rainSoundOff() {
-    rainSound.pause()
-  }
-  buttonSoundRain.addEventListener("click", function () {
-    if (buttonSoundRain.classList.contains("soundOn")) {
-      rainSoundOff()
-      buttonSoundRain.classList.remove("soundOn")
-    } else {
-      rainSoundOn()
-      buttonSoundRain.classList.add("soundOn")
-    }
-  })
-
-  //CoffeeShop Audio
-  const coffeeShopSound = new Audio("./sound/Cafeteria.wav")
-
-  function coffeeShopSoundOn() {
-    coffeeShopSound.play()
-  }
-
-  function coffeeShopSoundOff() {
-    coffeeShopSound.pause()
-  }
-  buttonSoundCoffeeShop.addEventListener("click", function () {
-    if (buttonSoundCoffeeShop.classList.contains("soundOn")) {
-      coffeeShopSoundOff()
-      buttonSoundCoffeeShop.classList.remove("soundOn")
-    } else {
-      coffeeShopSoundOn()
-      buttonSoundCoffeeShop.classList.add("soundOn")
-    }
-  })
-
-  //FiePlace Audio
-  const firePlaceSound = new Audio("./sound/Lareira.wav")
-
-  function firePlaceSoundOn() {
-    firePlaceSound.play()
-  }
-
-  function firePlaceSoundOff() {
-    firePlaceSound.pause()
-  }
-  buttonSoundFirePlace.addEventListener("click", function () {
-    if (buttonSoundFirePlace.classList.contains("soundOn")) {
-      firePlaceSoundOff()
-      buttonSoundFirePlace.classList.remove("soundOn")
-    } else {
-      firePlaceSoundOn()
-      buttonSoundFirePlace.classList.add("soundOn")
-    }
-  })
+console.log(Sounds)
