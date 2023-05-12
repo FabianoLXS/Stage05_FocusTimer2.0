373580b612be05dc3478c2780d0184e797b9dc06
Fabiano Luiz Santos <47800473+FabianoLXS@users.noreply.github.com>
2023-05-12 16:10:48 -0300

    git commit -m 'Refatorando código de áudio'

---
+++ js/elements.js
@@ -0,0 +1,26 @@
+export default function elements() {
+  //Timer variables
+  const buttonPlay = document.querySelector(".play")
+  const buttonStop = document.querySelector(".stop")
+  const buttonPlus = document.querySelector(".plus")
+  const buttonMinus = document.querySelector(".minus")
+  
+  //Sound variables
+  const buttonSoundForest = document.querySelector(".forestButton")
+  const buttonSoundRain = document.querySelector(".rainButton")
+  const buttonSoundCoffeeShop = document.querySelector(".coffeeShopButton")
+  const buttonSoundFirePlace = document.querySelector(".firePlaceButton")
+
+  return {
+    buttonPlay,
+    buttonStop,
+    buttonPlus,
+    buttonMinus,
+    buttonSoundForest,
+    buttonSoundRain,
+    buttonSoundCoffeeShop,
+    buttonSoundFirePlace,
+  }
+
+}
+
--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
-//Timer variables
-const buttonPlay = document.querySelector(".play")
-const buttonStop = document.querySelector(".stop")
-const buttonPlus = document.querySelector(".plus")
-const buttonMinus = document.querySelector(".minus")
+import elements from "./elements.js"
 
-//Sound variables
-const buttonSoundForest = document.querySelector(".forestButton")
-const buttonSoundRain = document.querySelector('.rainButton')
-const buttonSoundCoffeeShop = document.querySelector('.coffeeShopButton')
-const buttonSoundFirePlace = document.querySelector('.firePlaceButton')
+const Elements = elements({
+  buttonPlay,
+  buttonStop,
+  buttonPlus,
+  buttonMinus,
+  buttonSoundForest,
+  buttonSoundRain,
+  buttonSoundCoffeeShop,
+  buttonSoundFirePlace,
+})
 
 //Forest Audio
 const forestSound = new Audio(
