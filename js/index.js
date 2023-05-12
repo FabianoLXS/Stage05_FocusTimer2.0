e1f282f8ead0bc0e191ac9e4e21758c695568a42
Fabiano Luiz Santos <fab_luiz@msn.com>
2023-05-12 13:03:08 -0300

    commit

--- js/index.js
+++ js/index.js
@@ -1,13 +1,38 @@
+//Timer variables
 const buttonPlay = document.querySelector(".play")
 const buttonStop = document.querySelector(".stop")
 const buttonPlus = document.querySelector(".plus")
 const buttonMinus = document.querySelector(".minus")
 
+//Sound variables
+const buttonSoundForest = document.querySelector(".forestButton")
+const buttonSoundRain = document.querySelector('.rainButton')
+const buttonSoundCoffeeShop = document.querySelector('.coffeeShopButton')
+const buttonSoundFirePlace = document.querySelector('.firePlaceButton')
 
-buttonPlay.addEventListener("click", function () {
-  // buttonPlay.classList.add("pressed")
-  buttonPlay.classList.add("vermelhinho")
-  console.log(buttonPlay)
+//Button variables
+
+// buttonPlay.addEventListener("click", function () {
+//   // buttonPlay.classList.add("pressed")
+//   buttonPlay.classList.add("pressed")
+//   console.log(buttonPlay)
+// })
+
+
+//function Pressed Button Sound
+
+
+buttonSoundForest.addEventListener("click", function() {
+  pressButtonForest()
+  buttonSoundForest.classList.add("soundOn")
+
 })
 
+//Audio
+const forestSound = new Audio(
+  "./sound/Floresta.wav"
+)
 
+function pressButtonForest() {
+  forestSound.play()
+}
