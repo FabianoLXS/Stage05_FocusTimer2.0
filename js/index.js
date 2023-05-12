985953b2f42e5679367b22f85dad5fb33e63f08f
Fabiano Luiz Santos <fab_luiz@msn.com>
2023-05-11 23:52:34 -0300

    commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const buttonPlay = document.querySelector(".play")
+const buttonStop = document.querySelector(".stop")
+const buttonPlus = document.querySelector(".plus")
+const buttonMinus = document.querySelector(".minus")
+
+
+buttonPlay.addEventListener("click", function () {
+  // buttonPlay.classList.add("pressed")
+  buttonPlay.classList.add("vermelhinho")
+  console.log(buttonPlay)
+})
+
+
