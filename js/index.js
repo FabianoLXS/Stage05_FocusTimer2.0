79d612f4e9c8fbe66f0882174bd9b7b3322c71df
Fabiano Luiz Santos <47800473+FabianoLXS@users.noreply.github.com>
2023-05-29 16:19:32 -0300

    commit

--- js/index.js
+++ js/index.js
@@ -25,193 +25,190 @@ const pathStop = document.querySelector("path.stop")
 const pathIncrease = document.querySelector("path.increase")
 const pathDecrease = document.querySelector("path.decrease")
 
-
 // function Forest() {
-  //Forest Audio
-  const forestSound = new Audio("./sound/Floresta.wav")
+//Forest Audio
+const forestSound = new Audio("./sound/Floresta.wav")
 
-  function forestSoundOn() {
-    forestSound.play()
-  }
+function forestSoundOn() {
+  forestSound.play()
+}
 
-  function forestSoundOff() {
-    forestSound.pause()
+function forestSoundOff() {
+  forestSound.pause()
+}
+buttonSoundForest.addEventListener("click", function () {
+  if (buttonSoundForest.classList.contains("soundOn")) {
+    forestSoundOff()
+    buttonSoundForest.classList.remove("soundOn")
+    pathForest.classList.remove("soundOn")
+  } else {
+    forestSoundOn()
+    buttonSoundForest.classList.add("soundOn")
+    pathForest.classList.add("soundOn")
   }
-  buttonSoundForest.addEventListener("click", function () {
-    if (buttonSoundForest.classList.contains("soundOn")) {
-      forestSoundOff()
-      buttonSoundForest.classList.remove("soundOn")
-      pathForest.classList.remove("soundOn")
-     } else {
-      forestSoundOn()
-      buttonSoundForest.classList.add("soundOn")
-      pathForest.classList.add("soundOn")
-    }
-  })
+})
 // }
 
 // function Rain() {
-  //Rain Audio
-  const rainSound = new Audio("./sound/Chuva.wav")
+//Rain Audio
+const rainSound = new Audio("./sound/Chuva.wav")
 
-  function rainSoundOn() {
-    rainSound.play()
-  }
+function rainSoundOn() {
+  rainSound.play()
+}
 
-  function rainSoundOff() {
-    rainSound.pause()
+function rainSoundOff() {
+  rainSound.pause()
+}
+buttonSoundRain.addEventListener("click", function () {
+  if (buttonSoundRain.classList.contains("soundOn")) {
+    rainSoundOff()
+    buttonSoundRain.classList.remove("soundOn")
+    pathRain.classList.remove("soundOn")
+  } else {
+    rainSoundOn()
+    buttonSoundRain.classList.add("soundOn")
+    pathRain.classList.add("soundOn")
   }
-  buttonSoundRain.addEventListener("click", function () {
-    if (buttonSoundRain.classList.contains("soundOn")) {
-      rainSoundOff()
-      buttonSoundRain.classList.remove("soundOn")
-      pathRain.classList.remove("soundOn")
-    } else {
-      rainSoundOn()
-      buttonSoundRain.classList.add("soundOn")
-      pathRain.classList.add("soundOn")
-    }
-  })
+})
 // }
 
 // function CoffeeShop() {
-  //CoffeeShop Audio
-  const coffeeShopSound = new Audio("./sound/Cafeteria.wav")
+//CoffeeShop Audio
+const coffeeShopSound = new Audio("./sound/Cafeteria.wav")
 
-  function coffeeShopSoundOn() {
-    coffeeShopSound.play()
-  }
+function coffeeShopSoundOn() {
+  coffeeShopSound.play()
+}
 
-  function coffeeShopSoundOff() {
-    coffeeShopSound.pause()
+function coffeeShopSoundOff() {
+  coffeeShopSound.pause()
+}
+buttonSoundCoffeeShop.addEventListener("click", function () {
+  if (buttonSoundCoffeeShop.classList.contains("soundOn")) {
+    coffeeShopSoundOff()
+    buttonSoundCoffeeShop.classList.remove("soundOn")
+    pathCoffeeShop.classList.remove("soundOn")
+  } else {
+    coffeeShopSoundOn()
+    buttonSoundCoffeeShop.classList.add("soundOn")
+    pathCoffeeShop.classList.add("soundOn")
   }
-  buttonSoundCoffeeShop.addEventListener("click", function () {
-    if (buttonSoundCoffeeShop.classList.contains("soundOn")) {
-      coffeeShopSoundOff()
-      buttonSoundCoffeeShop.classList.remove("soundOn")
-      pathCoffeeShop.classList.remove("soundOn")
-    } else {
-      coffeeShopSoundOn()
-      buttonSoundCoffeeShop.classList.add("soundOn")
-      pathCoffeeShop.classList.add("soundOn")
-    }
-  })
+})
 // }
 
 // function FirePlace() {
-  //FiePlace Audio
-  const firePlaceSound = new Audio("./sound/Lareira.wav")
+//FiePlace Audio
+const firePlaceSound = new Audio("./sound/Lareira.wav")
 
-  function firePlaceSoundOn() {
-    firePlaceSound.play()
-  }
+function firePlaceSoundOn() {
+  firePlaceSound.play()
+}
 
-  function firePlaceSoundOff() {
-    firePlaceSound.pause()
+function firePlaceSoundOff() {
+  firePlaceSound.pause()
+}
+buttonSoundFirePlace.addEventListener("click", function () {
+  if (buttonSoundFirePlace.classList.contains("soundOn")) {
+    firePlaceSoundOff()
+    buttonSoundFirePlace.classList.remove("soundOn")
+    pathFire.classList.remove("soundOn")
+  } else {
+    firePlaceSoundOn()
+    buttonSoundFirePlace.classList.add("soundOn")
+    pathFire.classList.add("soundOn")
   }
-  buttonSoundFirePlace.addEventListener("click", function () {
-    if (buttonSoundFirePlace.classList.contains("soundOn")) {
-      firePlaceSoundOff()
-      buttonSoundFirePlace.classList.remove("soundOn")
-      pathFire.classList.remove("soundOn")
-    } else {
-      firePlaceSoundOn()
-      buttonSoundFirePlace.classList.add("soundOn")
-      pathFire.classList.add("soundOn")
-    }
-  })
+})
 // }
 
 //função set time
-time.addEventListener("click", function() {
+time.addEventListener("click", function () {
   if (pathPlay.classList != "pressedButton") {
     getMinutes()
   }
 })
 
 //função Play
-  buttonPlay.addEventListener("click", function () {
-    if (
-      pathPlay.classList.contains("pressedButton") &&
-      buttonPause.classList.contains("hide")
-    ) {
-      pathPlay.classList.remove("pressedButton")
-      buttonPlay.classList.add("hide")
-      buttonPause.classList.remove("hide")
-      buttonPause.classList.add("pressedButton")
-    } else {
-      pathPlay.classList.add("pressedButton")
-      countDown()
-    }
-  })
+buttonPlay.addEventListener("click", function () {
+  if (
+    pathPlay.classList.contains("pressedButton") &&
+    buttonPause.classList.contains("hide")
+  ) {
+    pathPlay.classList.remove("pressedButton")
+    buttonPlay.classList.add("hide")
+    buttonPause.classList.remove("hide")
+    buttonPause.classList.add("pressedButton")
+  } else {
+    pathPlay.classList.add("pressedButton")
+    countDown()
+  }
+})
 
-  //função Pause
-  buttonPause.addEventListener("click", function () {
-    if (
-      buttonPause.classList.contains("pressedButton")
-    ) {
-      buttonPause.classList.add("hide")
-      buttonPause.classList.remove("pressedButton")
-      buttonPlay.classList.remove("hide")
-      pathPlay.classList.add("pressedButton")
-    } else {
-      pathPause.classList.add("pressedButton")
-    }
-  })
-
+//função Pause
+buttonPause.addEventListener("click", function () {
+  if (buttonPause.classList.contains("pressedButton")) {
+    buttonPause.classList.add("hide")
+    buttonPause.classList.remove("pressedButton")
+    buttonPlay.classList.remove("hide")
+    pathPlay.classList.add("pressedButton")
+  } else {
+    pathPause.classList.add("pressedButton")
+  }
+})
 
 //função Stop
-  buttonStop.addEventListener("click", function () {
-    if (pathStop.classList.contains("pressedButton")) {
-      
-      pathStop.classList.remove("pressedButton")
-    } else {
-      buttonPause.classList.add("hide")
-      buttonPlay.classList.remove("hide")
-      pathPlay.classList.remove("pressedButton")
-      pathStop.classList.add("pressedButton")
-    }
-  })
+buttonStop.addEventListener("click", function () {
+  if (pathStop.classList.contains("pressedButton")) {
+    pathStop.classList.remove("pressedButton")
+  } else {
+    buttonPause.classList.add("hide")
+    buttonPlay.classList.remove("hide")
+    pathPlay.classList.remove("pressedButton")
+    pathStop.classList.add("pressedButton")
+  }
+})
 
 //função Increase
-  buttonIncrease.addEventListener("click", function () {
-    if (pathIncrease.classList.contains("pressedButton")) {
-      
-      pathIncrease.classList.remove("pressedButton")
-    } else {
-      pathIncrease.classList.add("pressedButton")
-     
-    }
-  })
+buttonIncrease.addEventListener("click", function () {
+  if (pathIncrease.classList.contains("pressedButton")) {
+    pathIncrease.classList.remove("pressedButton")
+  } else {
+    pathIncrease.classList.add("pressedButton")
+  }
+})
 
 //função Descrease
-  buttonDecrease.addEventListener("click", function () {
-    if (pathDecrease.classList.contains("pressedButton")) {
-      pathDecrease.classList.remove("pressedButton")
-    } else {
-      pathDecrease.classList.add("pressedButton")
-    }
-  })
+buttonDecrease.addEventListener("click", function () {
+  if (pathDecrease.classList.contains("pressedButton")) {
+    pathDecrease.classList.remove("pressedButton")
+  } else {
+    pathDecrease.classList.add("pressedButton")
+  }
+})
 
-  //countdown function
+//countdown function
 
 function getMinutes() {
-    let newMinutes = Number(prompt("Quantos minutos?"))
-    minutesDisplay.textContent = newMinutes || "00"
+  let newMinutes = Number(prompt("Quantos minutos?"))
+  minutesDisplay.textContent = newMinutes || "00"
 }
 
 function countDown() {
-  setTimeout(function() {
+  setTimeout(function () {
     let seconds = Number(secondsDisplay.textContent)
     let minutes = Number(minutesDisplay.textContent)
     if (seconds <= 0) {
-      seconds = 60 
+      seconds = 2
 
-      minutesDisplay.textContent = String(minutes - 1).padStart(2,"0")
+      minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
     }
-  
-    secondsDisplay.textContent = String(seconds - 1).padStart(2,"0")
+
+    if (minutes <= 0) {
+      console.log(seconds)
+      return
+    }
+
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
     countDown()
-    }, 1000)
-    
-  }
+  }, 1000)
+}
