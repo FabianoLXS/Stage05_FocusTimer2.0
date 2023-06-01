94955075de51aba3f91c7feb7995c93197efde9f
Fabiano Luiz Santos <47800473+FabianoLXS@users.noreply.github.com>
2023-06-01 17:11:25 -0300

    commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,43 @@
-import controls from "./controls.js"
-import elements from "./elements.js"
-import events from "./events.js"
-import sound from "./sound.js"
-import timer from "./timer.js"
+// import controls from "./controls.js"
+import elementsReference from "./elements.js"
+// import events from "./events.js"
+// import sound from "./sound.js"
+// import timer from "./timer.js"
+
+const elements = elementsReference.Elements({
+
+  time,
+  // minutesDisplay,
+  // secondsDisplay,
+  // buttonPlay,
+  // buttonPause,
+  // buttonStop,
+  // buttonIncrease,
+  // buttonDecrease,
+  // buttonSoundForest,
+  // buttonSoundRain,
+  // buttonSoundCoffeeShop,
+  // buttonSoundFirePlace,
+  // pathForest,
+  // pathRain,
+  // pathCoffeeShop,
+  // pathFire,
+  // pathPlay,
+  // pathStop,
+  // pathIncrease,
+  // pathDecrease,
+  // forestSound,
+  // rainSound,
+  // coffeeShopSound,
+  // firePlaceSound,
+  // kitchenTimerSound,
+  // minutes,
+  // button,
+  // ambienceSound,
+  // imagePath,
+  // seconds,
+  // soundType,
+  // timerTimeOut,
+})
+
+console.log(elements.timerVariables())
