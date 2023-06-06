1db8ba2fa3a30f5752f6520b8ff2edde41891963
Fabiano Luiz Santos <47800473+FabianoLXS@users.noreply.github.com>
2023-06-06 14:22:49 -0300

    git commit -m "final version of pomodoro 2.0"

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 
 import { Timer } from "./timer.js"
 import { Controls } from "./controls.js"
+import { Sound } from "./sound.js"
 
 //Timer variables
 const time = document.querySelector(".time")
@@ -47,79 +48,6 @@ let imagePath
 let seconds
 let soundType
 let timerTimeOut
-  
-
-  // Sound button listener
-  buttonSoundForest.addEventListener("click", function () {
-    soundType = "forest"
-    soundButton(soundType)
-    toggleAmbienceSound(button, imagePath, ambienceSound)
-  })
-  
-  buttonSoundRain.addEventListener("click", function () {
-    soundType = "rain"
-    soundButton(soundType)
-    toggleAmbienceSound(button, imagePath, ambienceSound)
-  })
-  
-  buttonSoundCoffeeShop.addEventListener("click", function () {
-    soundType = "coffeeShop"
-    soundButton(soundType)
-    toggleAmbienceSound(button, imagePath, ambienceSound)
-  })
-  
-  buttonSoundFirePlace.addEventListener("click", function () {
-    soundType = "firePlace"
-    soundButton(soundType)
-    toggleAmbienceSound(button, imagePath, ambienceSound)
-  })
-  
-
-  //Audio Funcions
-  function ambienceSoundPlay(ambienceSound) {
-    ambienceSound.play()
-    button.classList.add("soundOn")
-    imagePath.classList.add("soundOn")
-  }
-  
-  function ambienceSoundPause(ambienceSound) {
-    ambienceSound.pause()
-    button.classList.remove("soundOn")
-    imagePath.classList.remove("soundOn")
-  }
-  
-  function toggleAmbienceSound(button, imagePath, ambienceSound) {
-    if (button.classList.contains("soundOn")) {
-      ambienceSoundPause(ambienceSound)
-    } else {
-      ambienceSoundPlay(ambienceSound)
-    }
-  }
-  
-  function soundButton(soundType) {
-    switch (soundType) {
-      case "forest":
-        button = buttonSoundForest
-        imagePath = pathForest
-        ambienceSound = forestSound
-        break
-        case "rain":
-          button = buttonSoundRain
-          imagePath = pathRain
-          ambienceSound = rainSound
-        break
-        case "coffeeShop":
-          button = buttonSoundCoffeeShop
-          imagePath = pathCoffeeShop
-          ambienceSound = coffeeShopSound
-          break
-          case "firePlace":
-            button = buttonSoundFirePlace
-            imagePath = pathFire
-            ambienceSound = firePlaceSound
-            break
-          }
-        }     
 
 export const timer = Timer({
   time,
@@ -130,9 +58,7 @@ export const timer = Timer({
   timerTimeOut,
   minutes,
   seconds,
-  // controls,
 })
-
 
 const controls = Controls({
   buttonPlay,
@@ -150,5 +76,22 @@ const controls = Controls({
   minutesDisplay,
 })        
 
-
+const sound = Sound({
+  buttonSoundForest,
+  pathForest,
+  forestSound,
+  buttonSoundRain,
+  pathRain,
+  rainSound,
+  buttonSoundCoffeeShop,
+  pathCoffeeShop,
+  coffeeShopSound,
+  buttonSoundFirePlace,
+  pathFire,
+  firePlaceSound,
+  ambienceSound,
+  button,
+  imagePath,
+  soundType,
+})
 
